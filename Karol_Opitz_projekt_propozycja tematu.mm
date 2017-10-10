a4b9ddc27955f96fc70cb13e1d835614e63b7719
viq16 <32664958+viq16@users.noreply.github.com>
2017-10-10 11:48:54 +0200

    Add files via upload

---
+++ Karol_Opitz_projekt_propozycja tematu.mm
@@ -0,0 +1,188 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Propozycja tematu" FOLDED="false" ID="ID_1244863739" CREATED="1475734757982" MODIFIED="1507536227379" COLOR="#000000" STYLE="oval">
+<font SIZE="10" BOLD="false"/>
+<hook NAME="MapStyle" zoom="0.827">
+    <conditional_styles>
+        <conditional_style ACTIVE="true" LOCALIZED_STYLE_REF="styles.connection" LAST="false">
+            <node_periodic_level_condition PERIOD="2" REMAINDER="1"/>
+        </conditional_style>
+        <conditional_style ACTIVE="true" LOCALIZED_STYLE_REF="styles.topic" LAST="false">
+            <node_level_condition VALUE="2" MATCH_CASE="false" MATCH_APPROXIMATELY="false" COMPARATION_RESULT="0" SUCCEED="true"/>
+        </conditional_style>
+        <conditional_style ACTIVE="true" LOCALIZED_STYLE_REF="styles.subtopic" LAST="false">
+            <node_level_condition VALUE="4" MATCH_CASE="false" MATCH_APPROXIMATELY="false" COMPARATION_RESULT="0" SUCCEED="true"/>
+        </conditional_style>
+        <conditional_style ACTIVE="true" LOCALIZED_STYLE_REF="styles.subsubtopic" LAST="false">
+            <node_level_condition VALUE="6" MATCH_CASE="false" MATCH_APPROXIMATELY="false" COMPARATION_RESULT="0" SUCCEED="true"/>
+        </conditional_style>
+    </conditional_styles>
+    <properties fit_to_viewport="false;" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="Arial" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.connection" COLOR="#606060" STYLE="fork">
+<font NAME="Arial" SIZE="8" BOLD="false"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<node TEXT="Informacje&#xa;dodatkowe:" POSITION="left" ID="ID_1514867313" CREATED="1475735252789" MODIFIED="1507536052535" COLOR="#000000">
+<font SIZE="10" BOLD="false"/>
+<node TEXT="Imi&#x119; i Nazwisko" ID="ID_1660476461" CREATED="1475735285548" MODIFIED="1507627000717" COLOR="#000000">
+<font SIZE="10" BOLD="false"/>
+<node TEXT="Karol Opitz" ID="ID_57649070" CREATED="1507535973674" MODIFIED="1507627012015" COLOR="#000000">
+<font SIZE="10" BOLD="false"/>
+</node>
+<node TEXT=":Ilo&#x15b;&#x107; os&#xf3;b w zespole" ID="ID_958979278" CREATED="1507625835957" MODIFIED="1507627023490">
+<node TEXT="1" OBJECT="java.lang.Long|1" ID="ID_1733744844" CREATED="1507627061984" MODIFIED="1507627064830">
+<node TEXT="Projekt pod k&#x105;tem licencjatu" ID="ID_326796897" CREATED="1507627110913" MODIFIED="1507627126648"/>
+</node>
+</node>
+</node>
+<node TEXT="Tytu&#x142; pracy" ID="ID_1838796965" CREATED="1475735289214" MODIFIED="1507627045399" COLOR="#000000">
+<font SIZE="10" BOLD="false"/>
+<node TEXT="Symulator pi&#x142;karski" ID="ID_1455648695" CREATED="1507536024378" MODIFIED="1507627054373" COLOR="#000000">
+<font SIZE="10" BOLD="false"/>
+</node>
+</node>
+<node TEXT="Narz&#x119;dzia" ID="ID_153669799" CREATED="1475735357669" MODIFIED="1507536052554" COLOR="#000000">
+<font SIZE="10" BOLD="false"/>
+<node TEXT="Wymie&#x144; narz&#x119;dzia:&#xa;Przy ka&#x17c;dym narz&#x119;dziu&#xa;podaj odno&#x15b;nik do sci&#x105;gni&#x119;cia narz&#x119;dzia:" ID="ID_853296603" CREATED="1475735371676" MODIFIED="1507626198938" COLOR="#000000">
+<font SIZE="10" BOLD="false"/>
+<node TEXT="C#" ID="ID_1741622996" CREATED="1507535993290" MODIFIED="1507626213483" COLOR="#000000">
+<font SIZE="10" BOLD="false"/>
+</node>
+<node TEXT="Visual Studio" ID="ID_1780138331" CREATED="1507625952086" MODIFIED="1507626198938">
+<node TEXT="" ID="ID_661983907" CREATED="1507625978181" MODIFIED="1507626053238">
+<node TEXT="https://www.visualstudio.com" ID="ID_113118369" CREATED="1507626047290" MODIFIED="1507626175674" LINK="https://www.microsoft.com/pl-pl/store/b/visualstudio?invsrc=search&amp;OCID=AID620866_SEM_WIIQIQAAADC4Nny2:20171010090035:s&amp;s_kwcid=AL!4249!3!214086098325!e!!!!visual%20studio&amp;ef_id=WIIQIQAAADC4Nny2:20171010090035:s"/>
+</node>
+</node>
+</node>
+<node TEXT="Przyk&#x142;ad:" ID="ID_767778640" CREATED="1475735383733" MODIFIED="1507536052561" COLOR="#000000">
+<font SIZE="10" BOLD="false"/>
+<node TEXT="Free Plane&#xa;https://www.freeplane.org/wiki/index.php/Main_Page" ID="ID_1291349665" CREATED="1487767682437" MODIFIED="1507536052561" COLOR="#000000">
+<font SIZE="10" BOLD="false"/>
+</node>
+</node>
+</node>
+<node TEXT="Kr&#xf3;tki opis pracy" ID="ID_1532101756" CREATED="1475735395509" MODIFIED="1507536052566" COLOR="#000000">
+<font SIZE="10" BOLD="false"/>
+<node TEXT="Gra oparta na symulowanym i zale&#x17c;nym od u&#x17c;ytkownika rozwoju zawodnika i klubu. Gra b&#x119;dzie mia&#x142;a jedynie jedn&#x105; lig&#x119; wybranych przeze mnie dru&#x17c;yn. Mecze, statystyki, tabele, forma zespo&#x142;&#xf3;w, mistrzostwo i kilka innych." ID="ID_617154623" CREATED="1507536008570" MODIFIED="1507627512239" COLOR="#000000">
+<font SIZE="10" BOLD="false"/>
+</node>
+</node>
+</node>
+<node TEXT="Cz&#x119;&#x15b;&#x107; g&#x142;&#xf3;wna&#xa;propozycji" POSITION="right" ID="ID_1760426699" CREATED="1475735544988" MODIFIED="1505118242365" COLOR="#000000">
+<font SIZE="10" BOLD="false"/>
+<node TEXT="Graf&#xa;interfejsu&#xa;u&#x17c;ytkownika" ID="ID_604008343" CREATED="1475735556045" MODIFIED="1505118242369" COLOR="#000000">
+<font SIZE="10" BOLD="false"/>
+<node ID="ID_1216178367" CREATED="1475735570541" MODIFIED="1505118242372" COLOR="#000000"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#ff6666">Zaprojektuj graf:</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font SIZE="10" BOLD="false"/>
+<node TEXT="" ID="ID_999108705" CREATED="1507535924818" MODIFIED="1507535963799" COLOR="#000000">
+<font BOLD="false"/>
+<node TEXT="Interfejs u&#x17c;ytkownika" ID="ID_620603293" CREATED="1507627771370" MODIFIED="1507627923495">
+<node TEXT="Utworzenie zawodnika/menad&#x17c;era o danych podanych przez u&#x17c;ytkownika" ID="ID_1910660767" CREATED="1507627927063" MODIFIED="1507628030045">
+<node TEXT="Rozw&#xf3;j poszczeg&#xf3;lnych umiej&#x119;tno&#x15b;ci przy wykorzystaniu Punkt&#xf3;w Nauki uzyskiwanych stosownie do wynik&#xf3;w - im lepsze wyniki tym wi&#x119;cej pkt nauki" ID="ID_913543992" CREATED="1507628297750" MODIFIED="1507628432042"/>
+</node>
+<node TEXT="Wybranie dru&#x17c;yny z przygotowanej ligi" ID="ID_365035698" CREATED="1507627928580" MODIFIED="1507628066989">
+<node TEXT="Ka&#x17c;da dru&#x17c;yna charakteryzuje si&#x119; innymi statystykami i taktykami" ID="ID_1227002435" CREATED="1507628111998" MODIFIED="1507628205144"/>
+<node TEXT="Zale&#x17c;nie od wynik&#xf3;w u&#x17c;ytkownika b&#x119;dzie generowana aktualna forma kt&#xf3;ra b&#x119;dzie wp&#x142;ywa&#x142;a na bonusy w umiej&#x119;tno&#x15b;ciach dru&#x17c;yny" ID="ID_1322477368" CREATED="1507628232124" MODIFIED="1507628280624"/>
+</node>
+<node TEXT="Wybranie Avatara" ID="ID_1704697187" CREATED="1507628070437" MODIFIED="1507628107981">
+<node TEXT="Kilka dost&#x119;pnych avatar&#xf3;w dla u&#x17c;ytkownika" ID="ID_133880248" CREATED="1507628215835" MODIFIED="1507628227817"/>
+</node>
+<node TEXT="Przyciski" ID="ID_99196741" CREATED="1507628436864" MODIFIED="1507628448680">
+<node TEXT="Miejsca do wype&#x142;nienia pozwalaj&#x105;ce stworzy&#x107; zawodnika" ID="ID_1146412788" CREATED="1507628449491" MODIFIED="1507628488161"/>
+<node TEXT="Wyb&#xf3;r dru&#x17c;yny" ID="ID_526489995" CREATED="1507628490505" MODIFIED="1507628503291"/>
+<node TEXT="Wyb&#xf3;r avatara" ID="ID_1342666138" CREATED="1507628505551" MODIFIED="1507628516128"/>
+<node TEXT="Treningi dru&#x17c;yny" ID="ID_908929524" CREATED="1507628521103" MODIFIED="1507628532528"/>
+<node TEXT="Statystyki" ID="ID_939725579" CREATED="1507628534410" MODIFIED="1507628549782"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Pomoc&#xa;FreePlane" POSITION="left" ID="ID_1739939127" CREATED="1487768016141" MODIFIED="1505118242373" COLOR="#000000">
+<font SIZE="10" BOLD="false"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ol>
+      <li>
+        <font size="5">Dodanie w&#281;z&#322;a na tym samym poziomie: Enter </font>
+      </li>
+      <li>
+        <font size="5">Dodanie w&#281;z&#322;a poziom ni&#380;ej: Ins</font>
+      </li>
+      <li>
+        <font size="5">Wywo&#322;anie panelu formatowania: PPM na obszarze roboczym -&gt; panel formatowaia</font>
+      </li>
+    </ol>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</map>
